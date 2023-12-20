674bf18a2c4ccdaaa827935d1146727e7de75121
Matthew Zegar <matthewzegar@gmail.com>
2023-12-19 17:20:12 -0800

    Extract to projects page

---
+++ src/lib/projects/projects.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import Project from '$lib/projects/project.svelte';
+
+	const projects: App.Project[] = [
+		{
+			name: 'Node RAPIDS',
+			description: 'GPU-accelerated data-science libraries on Node.js',
+			skills: 'C++, TypeScript, CUDA',
+			href: 'https://github.com/rapidsai/node'
+		},
+		{
+			name: `Matt's Minigolf`,
+			description: 'Multiplayer minigolf game',
+			skills: 'C#, Razor, s&box',
+			href: 'https://github.com/sboxgame/minigolf'
+		},
+		{
+			name: 'Small Fish Site',
+			description: 'Small Fish, big dreams',
+			skills: 'Svelte, Tailwind',
+			href: 'https://smallfi.sh/'
+		},
+		{
+			name: 'OpenGL Model Viewer',
+			description: 'Basic OpenGL model viewer written in C++',
+			skills: 'OpenGL, C++',
+			href: 'https://smallfi.sh/'
+		},
+		{
+			name: 'Grubs',
+			description: 'Procedurally generated multiplayer worms-like game',
+			skills: 'C#, Razor, s&box',
+			href: 'https://github.com/apetavern/grubs'
+		},
+		{
+			name: 'Retter',
+			description: 'Reddit mobile app',
+			skills: 'Flutter, Dart',
+			href: 'https://github.com/matekdev/Retter'
+		},
+		{
+			name: 'Parkour Painters',
+			description: 'Retro multiplayer parkour game',
+			skills: 'C#, Razor, s&box',
+			href: 'https://github.com/apetavern/parkour-painters'
+		},
+		{
+			name: 'Turtle Links',
+			description: 'Dynamic link service provider',
+			skills: 'Svelte, Supabase, Tailwind',
+			href: 'https://github.com/chandertech/turtlelinks'
+		},
+		{
+			name: 'TTT',
+			description: 'Multiplayer mafia-esque game',
+			skills: 'C#, Razor, s&box',
+			href: 'https://github.com/matekdev/sbox-TTT'
+		}
+	];
+</script>
+
+<div class="py-4 font-roboto">
+	<p class="text-3xl pb-2 text-white">Projects & Contributions</p>
+	<div class="grid grid-cols-1 md:grid-cols-2">
+		{#each projects as project}
+			<Project {project} />
+		{/each}
+	</div>
+</div>

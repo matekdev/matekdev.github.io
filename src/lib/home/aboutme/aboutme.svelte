e05a1d20f88c424768ea02d303aeb54797258729
Matthew Zegar <matthewzegar@gmail.com>
2023-12-21 19:39:33 -0800

    Add about me section

---
+++ src/lib/home/aboutme/aboutme.svelte
@@ -0,0 +1,5 @@
+<p class="pt-4 font-roboto text-white">
+	Hey, I'm Matthew Zegar 👋. I'm currently working at Kijiji as a Software Developer. My main
+	interests are in game development, computer graphics, and software engineering. Check out my
+	projects page to see some of the work I've done in those areas.
+</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
+	import Aboutme from '$lib/home/aboutme/aboutme.svelte';
 	import Education from '$lib/home/education/education.svelte';
 	import Header from '$lib/home/header/header.svelte';
 	import Jobs from '$lib/home/jobs/jobs.svelte';
 </script>
 
 <Header />
+<Aboutme />
 <Jobs />
 <Education />

ccd308000ddc38bbc78eb91fb3ff38ea680a4e93
Matthew Zegar <matthewzegar@gmail.com>
2024-04-14 23:34:37 -0700

    Test out discord embeds

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
 <script>
+	import DiscordEmbed from '$lib/components/DiscordEmbed.svelte';
 	import Aboutme from '$lib/home/aboutme/aboutme.svelte';
 	import Education from '$lib/home/education/education.svelte';
 	import Header from '$lib/home/header/header.svelte';
 	import Jobs from '$lib/home/jobs/jobs.svelte';
 </script>
+
+<DiscordEmbed
+	title="Matthew Zegar's Page"
+	description="The personal website of Matthew Zegar"
+	href="/"
+	img="home/lr.jpg"
+/>
 
 <Header />
 <Aboutme />

3b037f84eecf000653b315622c760b3447c10308
Matthew Zegar <matthewzegar@gmail.com>
2024-05-20 17:09:28 -0700

    Attempt to add rss feed

---
+++ src/routes/rss/+server.ts
@@ -0,0 +1,52 @@
+export const prerender = true;
+
+export async function GET({ fetch }) {
+	const response = await fetch('/api/posts');
+	const posts: App.BlogPost[] = await response.json();
+
+	function escapeXML(s: string): string {
+		return s.replace(/[<>&'"]/g, (c) => {
+			switch (c) {
+				case '<':
+					return '&lt;';
+				case '>':
+					return '&gt;';
+				case '&':
+					return '&amp;';
+				case '"':
+					return '&quot;';
+				case "'":
+					return '&apos;';
+				default:
+					return c;
+			}
+		});
+	}
+
+	const headers = { 'Content-Type': 'application/xml' };
+	const xml = `
+		<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+			<channel>
+				<title>Matthew Zegar's Blog</title>
+				<description>Random thoughts</description>
+				<link>https://matek.dev/blog/</link>
+				<atom:link href="https://matek.dev/blog/rss.xml" rel="self" type="application/rss+xml"/>
+				${posts
+					.map(
+						(post) => `
+						<item>
+							<title>${escapeXML(post.title)}</title>
+							<description>${escapeXML(post.title)}</description>
+							<link>https://matek.dev/blog/${post.slug}</link>
+							<guid isPermaLink="true">https://matek.dev/blog/${post.slug}</guid>
+							<pubDate>${new Date(post.date).toUTCString()}</pubDate>
+						</item>
+					`
+					)
+					.join('')}
+			</channel>
+		</rss>
+	`.trim();
+
+	return new Response(xml, { headers });
+}

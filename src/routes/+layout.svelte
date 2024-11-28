fbd905e1415ea938417a62059cc0c495f901d5c9
Matthew Zegar <matthewzegar@gmail.com>
2024-11-27 21:13:46 -0800

    Grid bg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,22 @@
 	import '../syntax-highlight.css'; // https://github.com/PrismJS/prism-themes
 </script>
 
-<div class="fixed h-screen w-screen bg-jetblack" style="z-index: -5;" />
+<div class="fixed grid h-screen w-screen" style="z-index: -5;" />
 
 <div class="container mx-auto">
 	<Navbar />
 	<slot />
 </div>
+
+<style>
+	.grid {
+		background-color: #141416;
+		opacity: 1;
+		background-image: linear-gradient(
+				#63636b11 0.7000000000000001px,
+				transparent 0.7000000000000001px
+			),
+			linear-gradient(to right, #63636b11 0.7000000000000001px, #141416 0.7000000000000001px);
+		background-size: 14px 14px;
+	}
+</style>

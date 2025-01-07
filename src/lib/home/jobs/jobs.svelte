4fd41bd4272711421ccc5336404fe76227b6bfd9
Matthew Zegar <matthewzegar@gmail.com>
2025-01-06 19:44:11 -0800

    Update job title

--- src/lib/home/jobs/jobs.svelte
+++ src/lib/home/jobs/jobs.svelte
@@ -4,7 +4,7 @@
 	const jobs: App.Experience[] = [
 		{
 			name: 'Autodesk',
-			subheading: 'Software Developer',
+			subheading: 'Software Developer II',
 			location: 'Vancouver, Canada',
 			startDate: '2025-01-06',
 			href: 'https://www.autodesk.com/'
